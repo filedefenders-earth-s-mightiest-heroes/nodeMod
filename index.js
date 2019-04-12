279065a0f75b029c3d0285050acbef10fdc31340
Andrew Davis <andrew.bradley.davis@gmail.com>
2019-04-11 20:12:02 -0700

    convert relative to absolute paths & ensure file inputted is file not directory

--- index.js
+++ index.js
@@ -6,54 +6,58 @@ const chalk = require("chalk");
 const figlet = require("figlet");
 const shell = require("shelljs");
 const fs = require("fs");
+const resolve = require('path').resolve;
 
 const init = () => {
-    console.log(
-        chalk.red(
-            figlet.textSync('Check Me Out',{
-                font:"poison",
-                horizontalLayout:"default",
-                verticalLayout:"default"
-            })
-        )
-    );
+  console.log(
+      chalk.red(
+          figlet.textSync('Check Me Out', {
+            font: "poison",
+            horizontalLayout: "default",
+            verticalLayout: "default"
+          })
+      )
+  );
 };
 
 const askForFile = () => {
-    const q = [
-        {
-            name:"FILENAME",
-            type:"input",
-            message:"what is the path for the file that you would like to check?"
-        }
-    ];
-    return inquirer.prompt(q);
+  const q = [
+    {
+      name: "FILENAME",
+      type: "input",
+      message: "What is the path for the file that you would like to check?"
+    }
+  ];
+  return inquirer.prompt(q);
 };
 
 const createBuffer = (file) => {
-    fs.readFile(file, (err, data) => {
-        if (err) throw error;
-        console.log(data);
-        return data;
-    });
-}
+  fs.readFile(file, (err, data) => {
+    if (err) throw error;
+    console.log(data);
+    return data;
+  });
+};
 
 const run = async () => {
-    //show script intro
-    init ();
-    //ask user for a filepath
-    let file = "badFilePath";
-    //check to see if a file exists at the filepath
+  //show script intro
+  init();
+  //ask user for a filepath
+  let file = "badFilePath";
+  //check to see if a file exists at the filepath
+  file = await askForFile();
+  // Andrew - ensure file exists and is a file rather than directory, symlink, etc...
+  while (!fs.existsSync(`${file.FILENAME}` && !fs.statSync(file.FILENAME).isFile())) {
+    console.log('the filepath you entered was invalid, ensure it is a file not a directory);
     file = await askForFile();
-    while (!fs.existsSync(`${file.FILENAME}`)){
-        console.log('the filepath you entered was invalid, make sure you are entering the path relative to the current state of the terminal');
-        file = await askForFile();
-    };
+  }
+  // Andrew - resolve relative paths to absolute
+  file.FILENAME = resolve(file.FILENAME);
+  console.log(file.FILENAME);
 
-    //create a buffer for the file
-    console.log(typeof file.FILENAME);
-    const buffer = createBuffer(`${file.FILENAME}`);
+  //create a buffer for the file
+  // const buffer = createBuffer(`${file.FILENAME}`);
 
-    //attach the buffer to the body of the request
+  //attach the buffer to the body of the request
 };
 run();
