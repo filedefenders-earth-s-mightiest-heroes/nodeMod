5e34b2269a2d349adf3fed630f44bd2d21859a4c
Andrew Davis <andrew.bradley.davis@gmail.com>
2019-04-11 21:08:01 -0700

    fix logic error

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 'use strict';
 
+require('dotenv').config();
+
 const inquirer = require("inquirer");
 const chalk = require("chalk");
 const figlet = require("figlet");
 const shell = require("shelljs");
 const fs = require("fs");
 const resolve = require('path').resolve;
+const superagent = require('superagent');
+const http = require('https');
 
 const init = () => {
   console.log(
@@ -39,6 +43,17 @@ const createBuffer = (file) => {
   });
 };
 
+const uploadFile = (filepath) => {
+  superagent.post('https://api.metadefender.com/v4/file')
+    .set('apikey', `${process.env.API_KEY}`)
+    .set('Content-Type', 'application/octet-stream')
+    // .send("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"\"; filename=\"C:\\Users\\JeromeJoof\\Desktop\\kayjay.jpg\"\r\nContent-Type: application/pdf\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--")
+    .send(filepath)
+
+    .then(console.log)
+    .catch(console.log);
+};
+
 const run = async () => {
   //show script intro
   init();
@@ -46,9 +61,11 @@ const run = async () => {
   let file = "badFilePath";
   //check to see if a file exists at the filepath
   file = await askForFile();
+
   // Andrew - ensure file exists and is a file rather than directory, symlink, etc...
-  while (!fs.existsSync(`${file.FILENAME}` && !fs.statSync(file.FILENAME).isFile())) {
-    console.log('the filepath you entered was invalid, ensure it is a file not a directory);
+  while (!fs.existsSync(`${file.FILENAME}`) || !fs.statSync(file.FILENAME).isFile()) {
+    // console.log(fs.statSync(file.FILENAME).isFile());
+    console.log('the filepath you entered was invalid, ensure it is a file not a directory');
     file = await askForFile();
   }
   // Andrew - resolve relative paths to absolute
@@ -56,8 +73,12 @@ const run = async () => {
   console.log(file.FILENAME);
 
   //create a buffer for the file
+  // Andrew - waiting until after MVP to do it use this
   // const buffer = createBuffer(`${file.FILENAME}`);
 
   //attach the buffer to the body of the request
+
+  // Andrew - send filepath to Meta Defender API
+  // uploadFile(file.FILENAME);
 };
 run();
