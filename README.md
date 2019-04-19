b151539219246822bd38e8324f66c6e84103ec3b
jjblues86 <jeromejoof@yahoo.com>
2019-04-18 21:08:32 -0700

    complete

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # nodeMod
-command line front end node module
+![CF](http://i.imgur.com/7v5ASc8.png) command line front end node module
+==========================================================
+
+### Author: Jerome Joof, Richard bellamy, Connor Sihon, Andrew Davis, Gurinder Batth
+
+## Project Scope
+* Allow a user to send a file to the metadefender API through an easy to use terminal UX, which connects to an azure hosted api that will gather data from metadefender to let the user know whether they have a compromised file.
+
+
+## Links and Resources
+* [Repo](https://github.com/filedefenders-earth-s-mightiest-heroes)
+* [Heroku](https://dashboard.heroku.com/apps/check-me-out-n12)
+
+
+## Modules
+* upload-file.js
+* check-file.js
+
+
+## Installing 
+* Clone this repo > git clone 
+
+* enter the directory in your terminal
+   *  run "cd checkmeout-cli"
+   
+ * run npm i
+   
+* run "npm link" in your terminal 
+
+
+## Running the app
+
+* enter checkmeout in your terminal
+
+* enter the path of the file that needs to be checked relative to the current location in your terminal
+
+* this app runs the file path and sanitizes it. If the file is corrupted it lets you know.
--- upload-file.js
+++ upload-file.js
@@ -9,10 +9,60 @@ const resolve = require('path').resolve;
 const superagent = require('superagent');
 const http = require('https');
 const fs = require("fs");
+const endGame = require('./end-cli');
+
+// This are the allowed file extensions
+const support = ['PDF', 'DOCX', 'DOCM', 'DOTX', 'DOTM', 'XLSB', 'XLSX', 'XLSM', 'PPTX', 'PPTM', 'PPSX', 'RTF', 'BMP',
+     'PNG', 'JPG', 'JPEG', 'EPS', 'GIF', 'SVG', 'TIFF', 'TIF', 'HTM', 'HTML', 'XML', 'HWP', 'JTD', 'OTD', 'DWG'];
 
 
+// This function enables you to parse a relative file path and it accesses the api key and returns the id and file path
 module.exports.upload = (file) => {
-    superagent.post = ('https://check-me-out-n12.herokuapp.com/')
+
+    let ext = file.split('.');
+
+    if (support.includes(ext[1].toUpperCase())){
+        superagent.post('https://api.metadefender.com/v4/file')
+            .set('rule', 'sanitize')
+            //connor - if filetype scrubbing is supported, set rule for sanitize:
+            .set('apikey', `${process.env.API_KEY}`)
+            .set('Content-Type', 'application/octet-stream')
+            .send(file)
+            .then(res => {
+                getReport(res.body.data_id, file);
+            })
+            .catch(console.log);
+    }else{
+        superagent.post('https://api.metadefender.com/v4/file')
+        //connor - if filetype scrubbing is supported, set rule for sanitize:
+            .set('apikey', `${process.env.API_KEY}`)
+            .set('Content-Type', 'application/octet-stream')
+            .send(file)
+            .then(res => {
+                getReport(res.body.data_id, file);
+            })
+            .catch(console.log);
+    }
+
+    const getReport = (dataId, file) => {
+  superagent.get(`https://api.metadefender.com/v4/file/${dataId}`)
+    .set('apikey', `${process.env.API_KEY}`)
+    .then(res => {
+      let progress = res.body.scan_results.scan_all_result_a;
+      if (progress === 'In Progress'){
+        getReport(dataId, file);
+      }else{
+        let report = {
+          status: progress,
+          ID:dataId,
+          filePath:file
+        };
+        endGame.end(report);
+      }
+    })
+    .catch(console.log);
+};
+
 
 }
 
